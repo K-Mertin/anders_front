d7f16a870c8a0ce2cf6c1ddf15c4857bd21916e3
郭孟家(Mertin) <mertinkuo_tw@tw-moneysq.local>
2017-10-25 17:01:22 +0800

    'v1'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { VendorComponent } from './vendor/vendor.component';
+import { PurchaseComponent } from './purchase/purchase.component';
+import { HeaderComponent } from './shared';
+import { HomeComponent } from './home/home.component';
+
+const rootRoute: ModuleWithProviders = RouterModule.forRoot([
+  { path: 'product', component: ProductComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch:'full'},
+], { useHash: true });
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    VendorComponent,
+    PurchaseComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    rootRoute
   ],
   providers: [],
   bootstrap: [AppComponent]
